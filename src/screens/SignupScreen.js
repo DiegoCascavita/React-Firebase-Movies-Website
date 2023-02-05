e6502cdfd5fe477f13aa3711489bb0afbb009394
DiegoCascavita <sportscenter857@gmail.com>
2023-02-05 12:08:24 -0500

    login page fixed, working on profile page

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -41,10 +41,10 @@ function SignupScreen() {
       <h1>Sign Up</h1>
       <input ref={emailRef} placeholder='Email' type="email" />
       <input ref={passwordRef} placeholder='password' type="password"/>
-      <button type='submit' onClick={register}>Sign up</button>
+      <button type='submit' onClick={register} className='signUp_Button'>Sign up</button>
+      <button type='submit' onClick={signIn} >Login</button>
       <h4>
         <span className='signupScreen__gray'>New to Netflix ? </span>
-        <span className='signupScreen__link' onClick={signIn}>Sign Up now.</span>
       </h4>
     </form>
   </div>
